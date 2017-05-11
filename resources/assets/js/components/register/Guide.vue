5ac41753edf5681af6a3cb77f5db6e4715597f98
arzhed <arthur.simon@openmailbox.org>
2017-05-11 18:36:33 +0200

    homepage/welcome more consistent

---
+++ resources/assets/js/components/register/Guide.vue
@@ -0,0 +1,31 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Register</div>
+                <div class="panel-body">
+                    <form  class="form-horizontal" role="form" method="POST" action="{{ route('register') }}">
+
+                        
+
+                        <div class="form-group" v-if="true">
+                            <label for="password-confirm" class="col-md-4 control-label">Background</label>
+
+                            <div class="col-md-6">
+                                <textarea></textarea>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Register
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
