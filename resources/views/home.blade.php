34b72b89d459e82ac027db16a8ec7e86b0fd9982
arzhed <arthur.simon@ificlide.com>
2017-05-19 18:27:02 +0200

    clean

--- resources/views/home.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-<!-- 
-@section('content')
-    <div class="title text-black bold hp-vert-center">
-        Choose your path
-    </div>
-    <input type="text" class="form-control" placeholder="Try"></input>
-@endsection -->
